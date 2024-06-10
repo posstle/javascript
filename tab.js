d2ea9b13d84e485459019f4f5f74f19aa24f2164
posstle <posstle@naver.com>
2024-06-10 20:17:41 +0900

    탭 변경 방식 수정

--- tab.js
+++ tab.js
@@ -1,15 +1,10 @@
-const tabs = document.querySelectorAll('.tab-button')
-
-for (let i = 0; i < tabs.length; i++) {
-  let tabElement = tabs[i]
-
-  tabElement.addEventListener('click', (e) => {
-    changeTab(i)
-  })
-}
+document.querySelector('.list').addEventListener('click', (e) => {
+  changeTab(e.target.dataset.id)
+})
 
 function changeTab(num) {
   const content = document.querySelectorAll('.tab-content')
+  const tabs = document.querySelectorAll('.tab-button')
 
   for (let i = 0; i < content.length; i++) {
     content[i].classList.remove('show')
